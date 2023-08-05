e22389b70c905f8deed311443b4822b0b9afa7fe
Patrick Ziegler <ziegler.patrick@hotmail.de>
2023-08-05 13:51:29 +0200

    Cleanup tear-down methods to stabilize test cases
    The refresh call was added as part of the JUnit4 migration in an attempt
    to stabilize some of the failing test cases. Instead, this seems to
    have destabilized previously stable tests. In order to stabilize
    ProjectClassLoaderTest, we now call FileUtils.forceDelete() instead of
    FileUtils.deleteQuietly().
    We also need to ensure that resources are ALWAYS deleted once a test
    finishes, even if the test itself has failed. Otherwise those files may
    leak into the next test.

--- org.eclipse.wb.tests/src/org/eclipse/wb/tests/utils/ProjectClassLoaderTest.java
+++ org.eclipse.wb.tests/src/org/eclipse/wb/tests/utils/ProjectClassLoaderTest.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 Google, Inc.
+ * Copyright (c) 2011, 2023 Google, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -540,7 +540,7 @@ public class ProjectClassLoaderTest extends SwingModelTest {
 	public static String moveProjectIntoWorkspaceSubFolder() throws Exception {
 		String newProjectLocation = workspaceLocation + "/subFolder/Test";
 		// move project content
-		FileUtils.deleteQuietly(new File(newProjectLocation));
+		FileUtils.forceDelete(new File(newProjectLocation));
 		FileUtils.moveDirectory(
 				new File(m_project.getLocation().toPortableString()),
 				new File(newProjectLocation));
